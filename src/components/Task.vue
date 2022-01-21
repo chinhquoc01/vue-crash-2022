9ee8a3616ea920e7655d2a3640fb731ba1221b84
chinhquoc01 <qhanpuoc01@gmail.com>
2022-01-21 17:48:28 +0700

    delete, toggle reminder

---
+++ src/components/Task.vue
@@ -0,0 +1,49 @@
+<template>
+    <div @dblclick="$emit('toggle-reminder', task.id)" :class="[task.reminder? 'reminder' : '', 'task']">
+        <h3>
+            {{task.text}}
+            <i @click="onDelete(task.id)" class="fas fa-times"></i>
+        </h3>
+        <p>{{task.day}}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Task',
+    props: {
+        task: Object
+    },
+    methods: {
+        onDelete(id) {
+            // ! emit để gửi sự kiện từ components con lên component bố
+            // * sự kiện ở đây là 'delete-task' với tham số là 'id'
+            this.$emit('delete-task', id)
+        }
+    },
+    emits: ['toggle-reminder']
+}
+</script>
+
+<style scoped>
+.task {
+    background: #f4f4f4;
+    margin: 5px;
+    padding: 10px 20px;
+    cursor: pointer;
+}
+
+.task.reminder {
+    border-left: 5px solid green;
+}
+
+.task h3 {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+}
+
+.fas {
+    color: red;
+}
+</style>
